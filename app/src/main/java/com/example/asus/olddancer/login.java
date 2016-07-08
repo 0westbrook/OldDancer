51841efcb277a4b5d0c0382320959970021ab30e
0westbrook <616633185@qq.com>
2016-07-08 22:01:29 +0800

    杨国超提交

--- app/src/main/java/com/example/asus/olddancer/login.java
+++ app/src/main/java/com/example/asus/olddancer/login.java
@@ -2,15 +2,32 @@ package com.example.asus.olddancer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class login extends Activity {
+
+    private EditText login_username,login_password;
+    private TextView forget;
+    private Button register,login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
+        initInt();
 
+    }
+
+    private void initInt() {
+        login_username = (EditText) findViewById(R.id.username);
+        login_password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login_sure);
+        register = (Button) findViewById(R.id.login_register);
+        forget = (TextView) findViewById(R.id.forget);
     }
 }
